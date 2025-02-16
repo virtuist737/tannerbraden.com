902e2f0d41f19ce4e5d54d766c50d90ad435b6ad
virtuist <27257532-virtuist@users.noreply.replit.com>
2025-02-16 22:27:07 +0000

    Checkpoint after starting plan

---
+++ attached_assets/hobbies.js
@@ -0,0 +1,34 @@
+export const hobbies = {
+    categories: [
+      {
+        name: "Learning",
+        interests: [
+          "Español",
+          "Coding"
+        ]
+      },
+      {
+        name: "Musical Instruments",
+        instruments: [
+          "Piano",
+          "Guitar",
+          "Ukulele"
+        ]
+      },
+      {
+        name: "Sports",
+        activities: [
+          "Cycling",
+          "Hiking",
+          "Tennis"
+        ]
+      },
+      {
+        name: "Media",
+        interests: [
+          "Books",
+          "Podcasts"
+        ]
+      }
+    ]
+  };
