ba05a85d6f2b4799b5277a78cdd68a0fec7dc833
virtuist <27257532-virtuist@users.noreply.replit.com>
2025-02-16 13:17:46 +0000

    Checkpoint after starting plan

---
+++ attached_assets/website-content-part1.js
@@ -0,0 +1,65 @@
+// src/content/lifeStory.js
+export const lifeStory = {
+  sections: [
+    {
+      title: "Childhood",
+      content: `Who doesn't love the 90's? The era of VHS tapes, portable CD players, and dial-up internet. And let's not forget the great debate of our time: Playstation or Nintendo 64? As a 90's baby, I had a front-row seat to all of it.
+
+As for my family, my parents were baby-making machines, popping out seven kids in just nine years! Then, nine years later, I came into the picture. This big family and big age gap made for some interesting dynamics. I like to say that my mind is a decade older than my body.
+
+My family was also deeply religious. Like, devoutly so. We were highly involved in the local Mormon church and community, which had a big impact on me growing up. My beliefs have evolved drastically since then, but our religion instilled in me a strong desire to be a good person, make good decisions, and lend a helping hand. It also set me up for a life without harmful drugs or alcohol, which I'm very grateful for!
+
+Money was tight in our household. Most of my possessions were hand-me-downs or thrift store finds. Going out to eat or catching a movie was a luxury that only happened a few times a year. But you know what? It taught me to be grateful for the little things and work hard for what you want.
+
+Since I was knee-high to a grasshopper, I've had an insatiable curiosity for all things science and learning. While my buds begged to stay up late for Disney flicks, I pleaded for just one more hour of Nova space documentaries on the ol' PBS channel.
+
+Thanks to my love of learning and a burning desire to be the star student, I was always top of the class with perfect grades. In 4th grade I tested at a 12th+ grade reading proficiency! I remember being the last one left in the computer room as my test kept going into recess; and when the results printed out, the recommended reading list was empty - their system couldn't find any good books to match my age and reading level.
+
+But don't get me wrong, I wasn't just a bookworm. I also loved playing basketball, football (let's be real, it's more like handegg), tennis, and ultimate frisbee. Though I have to admit that I also spent countless hours slaying Bowser in Super Mario 64, battling it out in Super Smash Bros, and going on wild adventures in Sly Cooper, Kingdom Hearts, and many other video games.`
+    },
+    {
+      title: "Teenage Years",
+      content: `Looking back, I realize that I've always been a sucker for that amazing feeling you get when you conquer something that once seemed impossible. As a kid, video games scratched that itch for me, and they were a blast! But when I was just a wee lad of 10 years old, I realized that they failed to give me that real sense of accomplishment and fulfillment I craved. So, without a second thought, I decided to sell all of my video game gear - yep, every last bit of it - and jump headfirst into the wonderful world of music.
+
+From the age of 10 to 18, I became adept with multiple instruments including the piano, guitar, ukulele, cello, and drums. My obsession with music shaped my teenage years like nothing else. I joined orchestra, choir, and hung out with all the musically-inclined folks I could find. I was lucky enough to be chosen as one of the elite few juniors to join the Madrigals Choir in high school, and I even got to be the president of that choir during my senior year!
+
+But I have to give credit where credit is due. I was fortunate to have the best friends a guy could ask for during those formative years. They inspired me to be a better musician, student, athlete, and person. And let me tell you, I wouldn't be the person I am today without them. So, if there's one lesson I learned from my teenage journey, it's this: be wise in whom you choose to spend your time with, they influence you more than you realize!
+
+My love of learning and insatiable curiosity also continued to help me thrive in high school. I graduated top of class with a 3.98 GPA - just one "A-" to prove I'm not a robot - and with nearly an associate degree's worth of AP credits. In my junior year I scored a 32 on the ACT and I was later awarded top scholarships to both of my universities of choice.`
+    },
+    {
+      title: "Mormon Mission",
+      content: `Remember how I told you my family was super religious? Yeah, we're talking hardcore Mormon. And I was all in! I decided to follow in my dad and brothers' footsteps and dedicate two years of my life to serving as a missionary.
+
+So, a few months before graduating high school, I submitted my application to the church leaders asking them to assign me a mission. And guess what? They said I was supposed to go to Taiwan - a tiny island off the coast of China.
+
+Fast forward a few months, and I was at the Missionary Training Center learning how to speak Mandarin, teach the tenets of the Mormon church, and bring new converts into the fold. It wasn't easy, let me tell you. I was a pasty, 6'2" American kid in a sea of 5-foot-tall Taiwanese people. And don't even get me started on the language barrier!
+
+But I was determined to learn and grow, so I studied harder than I ever had before. And before I knew it, I was chatting away in Mandarin like a pro. I spent about 18 months in Taiwan, and by the time I left, I was practically a native. I had spent countless hours in the homes of Taiwanese families, learning about their culture, customs, and everyday lives. And the food! Oh my gosh, the food was amazing.
+
+I no longer believe in the religion I taught, and there were definitely some issues with how we were treated. But overall, I had an incredible experience in Taiwan. It taught me skills and lessons that have stuck with me to this day. Certainly not the least of which is the Mandarin Chinese language, which was the springboard that launched professional career.`
+    },
+    {
+      title: "College",
+      content: `As soon as I returned from my mission in Taiwan, I dove headfirst into college at Utah State University. Thanks to the Presidential Scholarship I was given, my tuition and fees were taken care of. Living expenses, however, were all on me. So I turned to my most valuable skill at the time: Mandarin Chinese.
+
+I was lucky to land a job at BOXFiSH Education, teaching English to Chinese kiddos online. The only catch? Time zones had it out for my beauty sleep. So I worked each day from 4am to 9am before my college classes.
+
+With this job, heavy church service, and a classload that nearly drove my academic advisor to resign, my schedule was like Tetris on expert mode. Fun and play took a back seat. But fear not, for my love affair with learning kept the college spark alive, studying was all the fun I needed.
+
+In fact, my love affair with learning got a little out of hand. Picture me like a kid in a candy store, eyes wide with wonder at every major on campus, determined to find the one that would maximize my opportunity to "make a dent in the universe." I ricocheted between majors like a caffeinated pinball on a quest for enlightenment.
+
+Here are just some of the majors I explored on my academic odyssey:
+- Aerospace Engineering
+- Physics
+- Pre-Med
+- Neuroscience
+- Business / Marketing
+- Behavioral Analysis
+
+Senior year snuck up on me and I found myself without a major to call my academic soulmate. Panic? Well, maybe a little. But I eventually decided to wrap up the whole undergraduate shebang and dive into the real world for some hands-on experience. So I emerged with a bachelor's degree in Interdisciplinary Studies and a transcript that's practically a highlight reel of my flirtations with a dozen different majors.
+
+Looking back now, this actually set myself up to be a very successful "generalist", rather than a specialist like most other graduates.`
+    }
+  ]
+};
---
+++ attached_assets/website-content-part5.js
@@ -0,0 +1,171 @@
+// src/content/experience.js (continued)
+export const additionalExperience = {
+  professional: [
+    {
+      company: "Logan Physical Therapy",
+      role: "Physical Therapy Aide",
+      type: "Part-time",
+      period: "Aug 2017 - Jan 2018",
+      duration: "5mos",
+      responsibilities: [
+        "Assisted and guided patients through rehabilitation exercises and therapies"
+      ]
+    },
+    {
+      company: "More Free Offers",
+      role: "Referral Agent",
+      type: "Freelance",
+      period: "Oct 2016 - Jan 2017",
+      duration: "3mos",
+      responsibilities: [
+        "Door-to-door sales for my father's small business"
+      ]
+    },
+    {
+      company: "In-N-Out Burger",
+      role: "Customer Services Associate",
+      type: "Part-time",
+      period: "July 2013 - Sep 2014",
+      duration: "1yr 2mos",
+      responsibilities: [
+        "Took orders, cleaned the restaurant, cooked the fries"
+      ]
+    },
+    {
+      company: "True North Business Development",
+      role: "Data Entry Associate",
+      type: "Freelance",
+      period: "Jan 2009 - Jul 2013",
+      duration: "4yrs 6mos",
+      responsibilities: [
+        "Completed data entry for my father's small business"
+      ]
+    }
+  ],
+  education: {
+    university: {
+      name: "Utah State University",
+      degree: "Bachelor of Arts in Interdisciplinary Studies",
+      graduation: "Class of 2020",
+      honors: "Summa Cum Laude",
+      credits: "158/120 credits completed",
+      subjects: [
+        "Calculus",
+        "Physics",
+        "Chemistry",
+        "Biology",
+        "Neuroscience",
+        "Behavioral Science",
+        "Psychology",
+        "Economics",
+        "Statistics",
+        "Finance",
+        "Marketing",
+        "Leadership"
+      ]
+    },
+    certificates: {
+      status: "Codecademy Certificates - In Progress..."
+    }
+  }
+};
+
+// src/content/recommendations.js
+export const recommendations = {
+  books: {
+    fantasy: [
+      {
+        author: "Brandon Sanderson",
+        series: [
+          {
+            name: "Mistborn",
+            books: [
+              "The Final Empire",
+              "The Well of Ascension",
+              "The Hero of Ages",
+              "The Allow of Law",
+              "Shadows of Self",
+              "The Bands of Mourning",
+              "The Lost Metal"
+            ]
+          },
+          {
+            name: "Stormlight Archives",
+            books: [
+              "The Way of Kings",
+              "Words of Radiance",
+              "Othbringer",
+              "Rhythm of War"
+            ]
+          }
+        ],
+        standalone: ["Elantris", "Warbreaker"]
+      },
+      {
+        author: "Patrick Rothfuss",
+        books: [
+          "The Name of the Wind",
+          "The Wise Man's Fear"
+        ]
+      }
+    ],
+    sciFi: [
+      {
+        author: "Andy Weir",
+        books: [
+          "Hail Mary",
+          "Artemis"
+        ]
+      },
+      {
+        title: "Ready Player One"
+      },
+      {
+        author: "Arthur C. Clarke",
+        books: [
+          "Rendezvous with Rama"
+        ]
+      },
+      {
+        author: "Richard Phillips",
+        books: [
+          "The Second Ship",
+          "Immune",
+          "Wormhole"
+        ]
+      },
+      {
+        author: "Dennis E. Taylor",
+        books: [
+          "We Are Legion",
+          "For We Are Many",
+          "All These Worlds",
+          "Heaven's River"
+        ]
+      },
+      {
+        author: "Neal Shusterman",
+        books: [
+          "Scythe",
+          "Thunder Head",
+          "The Toll"
+        ]
+      }
+    ],
+    nonFiction: [
+      "The Illustrated Happiness Trap by Russ Harris",
+      "Crucial Conversations",
+      "Steve Jobs Archive",
+      "Atomic Habits by James Clear",
+      "Mini Habits by Stephen Guise",
+      "Harry Potter and the Methods of Rationality by Eliezer Yudkowsky",
+      "How to Win Friends & Influence People by Dale Carnegie",
+      "Sapiens by Yuval Noah Harari",
+      "Thinking Fast and Slow by Daniel Kahneman",
+      "Naked Statistics by Charles Wheelan",
+      "The Elephant in the Brain",
+      "Designing Your Life",
+      "7 Habits of Highly Effective People"
+    ]
+  }
+};
